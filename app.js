10d543887bf3ffe8d5eb502149fa4deb9ed4f24c
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-03-18 00:44:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 "use strict";
-
-window.addEventListener("load", initApp);
-
-const newToDo = document.querySelector("#input-new-to-do");
-const list = document.querySelector("#to-do-list");
-
-function initApp() {
-    // event listener - button
-}
