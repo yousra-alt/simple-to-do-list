acd9cbffa138c48181223cda1b7fa51e19d938b0
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-03-18 00:12:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,27 +6,5 @@ const newToDo = document.querySelector("#input-new-to-do");
 const list = document.querySelector("#to-do-list");
 
 function initApp() {
-    document.querySelector("#btn-add-item").addEventListener("click", addToDo);
-}
-
-function addToDo() {
-    const listItem = document.createElement("li");
-    const listText = document.createElement("span");
-    const listBtn = document.createElement("button");
-
-    listItem.appendChild(listText);
-    listText.textContent = newToDo.value;
-    listItem.appendChild(listBtn);
-    listBtn.textContent = "Delete";
-    list.appendChild(listItem);
-
-    listBtn.addEventListener("click", removeToDo);
-
-    // reset input and focus
-    newToDo.value = "";
-    newToDo.focus();
-}
-
-function removeToDo() {
-    list.removeChild(this.parentNode);
+    // event listener - button
 }
