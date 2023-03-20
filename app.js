14d086bc6de4155539914e676693de3fc1a74fe0
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-03-20 15:28:43 +0100

    opgave 20.03 to do list

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+const list = document.querySelector("#to-do-list");
+const newToDO = document.querySelector("#input-new-to-do");
+
+function initApp () {
+console.log("loader");
+document.querySelector("#btn-add-item").addEventListener("click",addToDo);
+}
+
+
+function addToDo() {
+
+const html = /*html*/ `
+    <li>
+        <span>${newToDO.value}</span>
+        <button>Delete</button>
+    </li>
+    `;
+
+    list.insertAdjacentHTML("beforeend", html);
+    list.querySelector("li:last-child button").addEventListener("click", removeToDo);
+
+    newToDO.value = "";
+    newToDO.focus();
+
+    // anden metode
+//     console.log("YO")
+
+//     const listItem = document.createElement("li");
+//     const listText = document.createElement("span");
+//     const listBtn = document.createElement("button");
+
+// listText.textContent = newToDO.value;
+// listBtn.textContent ="Delete";
+
+// listItem.appendChild(listText);
+// listItem.appendChild(listBtn);
+
+// // append list to nll
+// list.appendChild(listItem)
+
+// // add click event to button
+// listBtn.addEventListener("click", removeToDo);
+
+// reset input and focus
+
+}
+
+function removeToDo () {
+    console.log("remove");
+    this.parentNode.remove();
+    
+}
+
